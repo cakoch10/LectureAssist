96fd74eb3109a81bdc77b75995d0c2cb51ad3f36
Raymone1 <raymonelawyer@gmail.com>
2017-09-16 14:48:59 -0400

    Add files via upload

--- app2.py
+++ app2.py
@@ -16,15 +16,26 @@ def prof():
 def student():
     return render_template('student.html')
 
+@app.route('/')
+def main():
+    return render_template ('index.html')
+
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
 
-
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
+
+@socketio.on('Upvote Feed Item')
+def upvote(feedItem):
+    print('Feed Item Upvoted')
+
+@socketio.on('Submit Feed Item')
+def submit_item_to_feed(feedItem):
+    print('Feed Item Submitted: ' + feedItem)
 
 
 if __name__ == '__main__':
