322acdfc1ca724820d03acf1be3cad3a1ade9848
Wes T4N <wesleyt1998@gmail.com>
2017-09-16 15:32:41 -0400

    button + textfield working

--- js/main.js
+++
@@ -1,8 +0,0 @@
-Vue.component('input-area', {
-  template: '<textarea class="question-input pane" rows="4"></textarea>'
-});
-window.onload = function () {
-  var app = new Vue({
-    el: '#app'
-  });
-}
---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+var textBox = Vue.component('input-area', {
+  template: '',
+});
+var sendBtn = Vue.component('send-btn', {
+  template: '',
+});
+window.onload = function () {
+
+  var app = new Vue({
+    el: '#app',
+    components: {
+      textBox,
+      sendBtn
+    },
+    data: {
+      question: ""
+    },
+    methods: {
+      sendQuestion: function(question) {
+        alert(question);
+      }
+    }
+  });
+  var socket = io.connect('http://' + document.domain + ':' + location.port);
+  socket.on('connect', function () {
+    socket.emit('my event', {
+      data: 'I\'m connected!'
+    });
+  });
+
+  
+}
