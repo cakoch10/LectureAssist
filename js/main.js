6bbd6b40a87e81ca61541212beb58e6b2af6364a
Wes T4N <wesleyt1998@gmail.com>
2017-09-16 14:27:32 -0400

    working on input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+Vue.component('input-area', {
+  template: '<textarea class="question-input pane" rows="4"></textarea>'
+});
 window.onload = function () {
   var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
+    el: '#app'
   });
 }
