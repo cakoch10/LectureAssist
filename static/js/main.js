3f0554c1de96e06ae23dd90f162c2c7f59b71e0b
Wes T4N <wesleyt1998@gmail.com>
2017-09-16 21:36:09 -0400

    frontend improvements

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,34 @@
-var textBox = Vue.component('input-area', {
-  template: '',
+var feedEntry = Vue.component('feed-entry', {
+  template: `<div class="feed-entry">
+  <div class="upvote-count">{{ counter }}</div>
+<button class="upvote-btn" v-on:click="thumbsUp" :disabled=disabled><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+  <div class="message">{{ message }}</div>
+  </div>`,
+  data: 
+    function() {
+    return {
+      counter: 0,
+      message: "",
+      disabled: false, 
+    }
+  },
+  methods: {
+    thumbsUp: function(message) {
+      this.counter += 1;
+      this.disabled = true;
+    }
+  },
+  computed: {
+  }
 });
-var sendBtn = Vue.component('send-btn', {
-  template: '',
-});
+
 window.onload = function () {
   var socket = io();
-  socket.on('connect', function () {
-    socket.emit('my event', {
-      data: 'I\'m connected!'
-    });
-  });
+
   var app = new Vue({
     el: '#app',
     components: {
-      textBox,
-      sendBtn
+      feedEntry
     },
     data: {
       question: ""
@@ -26,7 +39,14 @@ window.onload = function () {
       }
     }
   });
+  socket.on('connect', function () {
+    socket.emit('my event', {
+      data: 'I\'m connected!'
+    });
+  });
+  socket.on('message', function (msg) {
 
+  });
 
   
 }
